180e2cc86cbd41bf68f11c9a3bfae3c61f4681a5
aqelkp <aqel123@gmail.com>
2015-01-31 00:46:26 +0530

    Initial commit

---
+++ app/src/main/java/in/aqel/myinstiapp/Constants.java
@@ -0,0 +1,12 @@
+package in.aqel.myinstiapp;
+
+/**
+ * Created by aqel on 31/1/15.
+ */
+public class Constants {
+    public static String[] hostels = {
+        "Alakananda","Brahmaputra","Cauvery","Ganga","Godavari","Jamuna",
+        "Krishna","Mahanadhi","Mandakini","Narmada","Pamba","Saraswathi","Sarayu" ,
+        "Sindhu","Sharavati","Sabarmati","Tamraparani","Tapti"
+    };
+}
