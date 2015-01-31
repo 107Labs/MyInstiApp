f3bebcce8a9f656dbde98191eeff61d83ccb0bbf
aqelkp <aqel123@gmail.com>
2015-01-31 15:16:26 +0530

    Updated UI for register Activity Relative layout changed to Linear Layout

--- app/src/main/java/in/aqel/myinstiapp/Register.java
+++ app/src/main/java/in/aqel/myinstiapp/Register.java
@@ -1,25 +1,55 @@
 package in.aqel.myinstiapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.common.AccountPicker;
 
 
-public class Register extends ActionBarActivity {
+public class Register extends ActionBarActivity  {
+
+    static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         Spinner hostels = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.hostels, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         hostels.setAdapter(adapter);
+        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickUserAccount();
+            }
+        });
+        Account[] accounts = AccountManager.get(this).getAccounts();
+/*       Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                String possibleEmail = account.name;
+
+                Log.d("Emails are ", possibleEmail);
+            }
+        }
+        */
+        String email = accounts[0].name;
+        Log.d("Email that is", email);
+
     }
 
 
@@ -44,4 +74,32 @@ public class Register extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    }
+
+    String mEmail; // Received from newChooseAccountIntent(); passed to getToken()
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            // Receiving a result from the AccountPicker
+            if (resultCode == RESULT_OK) {
+                mEmail = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                // With the account name acquired, go get the auth token
+                Log.d("Email is ", mEmail);
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The account picker dialog closed without selecting an account.
+                // Notify users that they must pick an account to proceed.
+                Toast.makeText(this, "Please pick a user name", Toast.LENGTH_SHORT).show();
+            }
+        }
+        // Later, more code will go here to handle the result from some exceptions...
+    }
+
 }
